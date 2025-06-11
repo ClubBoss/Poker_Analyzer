c5f61f73b500ff7df2b69558c94e464e896f29be
ClubBoss <esalimzade@hotmail.com>
2025-06-11 05:32:55 +0400

    Refactor pot system with PotCalculator

---
+++ lib/helpers/pot_calculator.dart
@@ -0,0 +1,17 @@
+import '../models/action_entry.dart';
+import '../models/street_investments.dart';
+
+/// Utility for computing pot sizes for each street.
+class PotCalculator {
+  /// Returns the cumulative pot for each street based on [investments].
+  List<int> calculatePots(
+      List<ActionEntry> actions, StreetInvestments investments) {
+    final pots = List<int>.filled(4, 0);
+    int cumulative = 0;
+    for (int s = 0; s < pots.length; s++) {
+      cumulative += investments.getTotalInvestedOnStreet(s);
+      pots[s] = cumulative;
+    }
+    return pots;
+  }
+}
