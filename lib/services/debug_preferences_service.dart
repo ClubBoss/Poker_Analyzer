932ed515b37f59654944b2d024313f091ea323c8
ClubBoss <esalimzade@hotmail.com>
2025-06-14 22:26:00 +0400

    refactor: centralize debug preferences

--- lib/services/debug_preferences_service.dart
+++ lib/services/debug_preferences_service.dart
@@ -9,10 +9,14 @@ class DebugPreferencesService extends ChangeNotifier {
 
   bool _snapshotRetentionEnabled = true;
   int _processingDelay = 500;
+  bool _sortBySpr = false;
+  String _searchQuery = '';
   bool _queueResumed = false;
 
   bool get snapshotRetentionEnabled => _snapshotRetentionEnabled;
   int get processingDelay => _processingDelay;
+  bool get sortBySpr => _sortBySpr;
+  String get searchQuery => _searchQuery;
   bool get queueResumed => _queueResumed;
 
   Future<void> loadSnapshotRetentionPreference() async {
@@ -37,6 +41,28 @@ class DebugPreferencesService extends ChangeNotifier {
     notifyListeners();
   }
 
+  Future<void> loadSortBySprPreference() async {
+    _sortBySpr = await _prefs.getSortBySpr();
+    notifyListeners();
+  }
+
+  Future<void> setSortBySpr(bool value) async {
+    await _prefs.setSortBySpr(value);
+    _sortBySpr = value;
+    notifyListeners();
+  }
+
+  Future<void> loadSearchQueryPreference() async {
+    _searchQuery = await _prefs.getSearchQuery();
+    notifyListeners();
+  }
+
+  Future<void> setSearchQuery(String value) async {
+    await _prefs.setSearchQuery(value);
+    _searchQuery = value;
+    notifyListeners();
+  }
+
   Future<void> loadQueueResumedPreference() async {
     final prefs = await SharedPreferences.getInstance();
     _queueResumed = prefs.getBool(_queueResumedKey) ?? false;
@@ -47,6 +73,15 @@ class DebugPreferencesService extends ChangeNotifier {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool(_queueResumedKey, value);
     _queueResumed = value;
+    notifyListeners();
+  }
+
+  Future<void> clearAll() async {
+    await _prefs.clearAll();
+    _snapshotRetentionEnabled = await _prefs.getSnapshotRetentionEnabled();
+    _processingDelay = await _prefs.getProcessingDelay();
+    _sortBySpr = await _prefs.getSortBySpr();
+    _searchQuery = await _prefs.getSearchQuery();
     notifyListeners();
   }
 }
