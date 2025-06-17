1f55dad1a7c490127e8515324b7935eb6fcbe7a7
ClubBoss <esalimzade@hotmail.com>
2025-06-17 23:10:48 +0400

    Add training generator with tournament metadata

---
+++ import_export/training_generator.dart
@@ -0,0 +1,36 @@
+import 'package:poker_ai_analyzer/models/saved_hand.dart';
+import 'package:poker_ai_analyzer/models/training_spot.dart';
+
+/// Generates [TrainingSpot]s from [SavedHand] instances.
+class TrainingGenerator {
+  /// Converts [hand] into a [TrainingSpot] preserving tournament metadata.
+  TrainingSpot generateFromSavedHand(SavedHand hand) {
+    return TrainingSpot(
+      playerCards: [
+        for (final list in hand.playerCards) List.of(list),
+      ],
+      boardCards: List.of(hand.boardCards),
+      actions: List.of(hand.actions),
+      heroIndex: hand.heroIndex,
+      numberOfPlayers: hand.numberOfPlayers,
+      playerTypes: [
+        for (int i = 0; i < hand.numberOfPlayers; i++)
+          hand.playerTypes?[i] ?? PlayerType.unknown,
+      ],
+      positions: [
+        for (int i = 0; i < hand.numberOfPlayers; i++)
+          hand.playerPositions[i] ?? '',
+      ],
+      stacks: [
+        for (int i = 0; i < hand.numberOfPlayers; i++)
+          hand.stackSizes[i] ?? 0,
+      ],
+      tournamentId: hand.tournamentId,
+      buyIn: hand.buyIn,
+      totalPrizePool: hand.totalPrizePool,
+      numberOfEntrants: hand.numberOfEntrants,
+      gameType: hand.gameType,
+    );
+  }
+}
+
