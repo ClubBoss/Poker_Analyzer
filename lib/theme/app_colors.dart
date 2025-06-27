736f14383061a1b14a56784f1fd4fcdb7be053d4
ClubBoss <esalimzade@hotmail.com>
2025-06-27 17:24:21 +0400

    Add accent color and improve weekday activity chart

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const background = Color(0xFF1B1C1E);
   static const cardBackground = Color(0xFF2A2B2E);
+  static const accent = Colors.orangeAccent;
 }
