fca9aabffbc7d93dcbe865995a747ca2b7a4cb5d
ClubBoss <esalimzade@hotmail.com>
2025-06-28 20:14:17 +0400

    Add async evaluation executor with caching

---
+++ lib/models/eval_request.dart
@@ -0,0 +1,8 @@
+import "training_spot.dart";
+class EvalRequest {
+  final String hash;
+  final TrainingSpot spot;
+  final String action;
+
+  EvalRequest({required this.hash, required this.spot, required this.action});
+}
