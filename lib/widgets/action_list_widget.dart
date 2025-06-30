22c384c2058b5ac1f113b31c47b0a9189896dd6c
ClubBoss <esalimzade@hotmail.com>
2025-06-30 06:59:58 +0400

    fix equity threshold calc

--- lib/widgets/action_list_widget.dart
+++ lib/widgets/action_list_widget.dart
@@ -116,9 +116,8 @@ class _ActionListWidgetState extends State<ActionListWidget> {
                   final pa = entry.potAfter;
                   final po = entry.potOdds;
                   if (pa == 0 || po == null) return null;
-                  final tc = pa * po / 100;
-                  if (tc == 0) return null;
-                  return 100 * tc / pa;
+                  if (po == 0) return null;
+                  return 100 * po / (po + 100);
                 })()
               : null;
           return AlertDialog(
