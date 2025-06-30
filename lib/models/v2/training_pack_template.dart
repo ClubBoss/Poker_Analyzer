60bc7906e8d8bbf87ffaa8a0bf15f8bd99c430ee
ClubBoss <esalimzade@hotmail.com>
2025-06-30 23:18:38 +0400

    Add game type filtering in template library

--- lib/models/v2/training_pack_template.dart
+++ lib/models/v2/training_pack_template.dart
@@ -4,12 +4,14 @@ class TrainingPackTemplate {
   final String id;
   String name;
   String description;
+  String gameType;
   List<TrainingPackSpot> spots;
 
   TrainingPackTemplate({
     required this.id,
     required this.name,
     this.description = '',
+    this.gameType = 'tournament',
     List<TrainingPackSpot>? spots,
   }) : spots = spots ?? [];
 
@@ -17,12 +19,14 @@ class TrainingPackTemplate {
     String? id,
     String? name,
     String? description,
+    String? gameType,
     List<TrainingPackSpot>? spots,
   }) {
     return TrainingPackTemplate(
       id: id ?? this.id,
       name: name ?? this.name,
       description: description ?? this.description,
+      gameType: gameType ?? this.gameType,
       spots: spots ?? List<TrainingPackSpot>.from(this.spots),
     );
   }
@@ -32,6 +36,7 @@ class TrainingPackTemplate {
       id: json['id'] as String? ?? '',
       name: json['name'] as String? ?? '',
       description: json['description'] as String? ?? '',
+      gameType: json['gameType'] as String? ?? 'tournament',
       spots: [
         for (final s in (json['spots'] as List? ?? []))
           TrainingPackSpot.fromJson(Map<String, dynamic>.from(s))
@@ -43,6 +48,7 @@ class TrainingPackTemplate {
         'id': id,
         'name': name,
         'description': description,
+        'gameType': gameType,
         if (spots.isNotEmpty) 'spots': [for (final s in spots) s.toJson()],
       };
 }
