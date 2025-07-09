1c92aade5388411069a1f59e960b891852f4fe35
ClubBoss <esalimzade@hotmail.com>
2025-07-10 02:22:20 +0400

    refactor: unify progress chart colors

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -5,4 +5,8 @@ class AppColors {
   static const cardBackground = Color(0xFF2A2B2E);
   static const accent = Colors.orangeAccent;
   static final errorBg = Colors.red.withOpacity(.2);
+  static const evPre = Colors.orangeAccent;
+  static const evPost = Colors.deepOrangeAccent;
+  static const icmPre = Colors.lightBlueAccent;
+  static const icmPost = Colors.blueAccent;
 }
