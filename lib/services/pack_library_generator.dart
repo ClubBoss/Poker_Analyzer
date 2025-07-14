2ae39373f1f3009c886803d42e74cb6000cc3ba7
ClubBoss <esalimzade@hotmail.com>
2025-07-14 13:59:37 +0400

    feat: add pack library generator

---
+++ lib/services/pack_library_generator.dart
@@ -0,0 +1,51 @@
+import '../models/v2/training_pack_template.dart';
+import '../models/v2/hero_position.dart';
+import 'spot_template_engine.dart';
+
+class PackLibraryGenerator {
+  final SpotTemplateEngine engine;
+  final List<TrainingPackTemplate> _packs = [];
+
+  PackLibraryGenerator({SpotTemplateEngine? engine})
+      : engine = engine ?? SpotTemplateEngine();
+
+  List<TrainingPackTemplate> get packs => List.unmodifiable(_packs);
+
+  Future<void> generateAll({
+    List<HeroPosition>? heroPositions,
+    List<HeroPosition>? villainPositions,
+    List<List<int>>? stackRanges,
+    List<String>? actionTypes,
+    bool includeIcm = true,
+  }) async {
+    _packs.clear();
+    final heroes = heroPositions ?? HeroPosition.values;
+    final villains = villainPositions ?? HeroPosition.values;
+    final ranges = stackRanges ??
+        const [
+          [10],
+          [15],
+          [20]
+        ];
+    final actions = actionTypes ?? const ['push', 'callPush', 'minraiseFold'];
+    final modes = includeIcm ? const [false, true] : const [false];
+    for (final hero in heroes) {
+      for (final vill in villains) {
+        for (final r in ranges) {
+          for (final type in actions) {
+            for (final icm in modes) {
+              final tpl = await engine.generate(
+                heroPosition: hero,
+                villainPosition: vill,
+                stackRange: r,
+                actionType: type,
+                withIcm: icm,
+              );
+              _packs.add(tpl);
+            }
+          }
+        }
+      }
+    }
+  }
+}
