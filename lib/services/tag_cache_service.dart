68cd0817708f18e7da063c1e1478a5c8093c93ad
ClubBoss <esalimzade@hotmail.com>
2025-07-16 18:18:54 +0400

    feat: add tag cache service

--- lib/services/tag_cache_service.dart
+++ lib/services/tag_cache_service.dart
@@ -1,78 +1,47 @@
-import 'package:flutter/foundation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
-import '../models/training_pack.dart';
-import '../models/training_pack_template.dart';
-import 'template_storage_service.dart';
-import 'training_pack_storage_service.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class TagCacheService extends ChangeNotifier {
-  static const _tagsKey = 'tag_cache_tags';
-  static const _catsKey = 'tag_cache_cats';
+  TagCacheService._();
+  static final TagCacheService instance = TagCacheService._();
+  factory TagCacheService() => instance;
 
-  final TemplateStorageService templates;
-  final TrainingPackStorageService packs;
-  List<String> _popularTags = [];
-  List<String> _popularCategories = [];
+  Map<String, int> topTags = {};
+  Map<String, int> topCategories = {};
+  bool _loaded = false;
 
-  List<String> get popularTags => List.unmodifiable(_popularTags);
-  List<String> get popularCategories => List.unmodifiable(_popularCategories);
+  List<String> get popularTags => List.unmodifiable(topTags.keys);
+  List<String> get popularCategories => List.unmodifiable(topCategories.keys);
 
-  TagCacheService({required this.templates, required this.packs}) {
-    templates.addListener(_update);
-    packs.addListener(_update);
-    _load();
-    _update();
-  }
-
-  Future<void> _load() async {
-    final prefs = await SharedPreferences.getInstance();
-    _popularTags = prefs.getStringList(_tagsKey) ?? [];
-    _popularCategories = prefs.getStringList(_catsKey) ?? [];
-  }
-
-  Future<void> updateFrom(
-      List<TrainingPackTemplate> tpls, List<TrainingPack> packsList) async {
-    final tagCounts = <String, int>{};
-    final catCounts = <String, int>{};
-    for (final t in tpls) {
-      for (final tag in t.tags) {
-        tagCounts.update(tag, (v) => v + 1, ifAbsent: () => 1);
-      }
-      for (final h in t.hands) {
-        final c = h.category;
-        if (c != null && c.isNotEmpty) {
-          catCounts.update(c, (v) => v + 1, ifAbsent: () => 1);
-        }
-      }
+  Future<void> load() async {
+    if (_loaded) return;
+    _loaded = true;
+    try {
+      final raw =
+          await rootBundle.loadString('assets/packs/v2/tag_frequencies.json');
+      final data = jsonDecode(raw) as Map<String, dynamic>;
+      topTags = Map<String, int>.from(
+        (data['tags'] as Map).map(
+          (k, v) => MapEntry(k.toString(), (v as num).toInt()),
+        ),
+      );
+      topCategories = Map<String, int>.from(
+        (data['categories'] as Map).map(
+          (k, v) => MapEntry(k.toString(), (v as num).toInt()),
+        ),
+      );
+    } catch (_) {
+      topTags = {};
+      topCategories = {};
     }
-    for (final p in packsList) {
-      for (final tag in p.tags) {
-        tagCounts.update(tag, (v) => v + 1, ifAbsent: () => 1);
-      }
-      final c = p.category;
-      if (c.isNotEmpty) {
-        catCounts.update(c, (v) => v + 1, ifAbsent: () => 1);
-      }
-    }
-    final tags = tagCounts.keys.toList()
-      ..sort((a, b) => tagCounts[b]!.compareTo(tagCounts[a]!));
-    final cats = catCounts.keys.toList()
-      ..sort((a, b) => catCounts[b]!.compareTo(catCounts[a]!));
-    _popularTags = tags.take(20).toList();
-    _popularCategories = cats.take(20).toList();
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setStringList(_tagsKey, _popularTags);
-    await prefs.setStringList(_catsKey, _popularCategories);
     notifyListeners();
   }
 
-  Future<void> _update() => updateFrom(templates.templates, packs.packs);
+  List<String> getPopularTags({int limit = 10}) =>
+      topTags.keys.take(limit).toList();
 
-  @override
-  void dispose() {
-    templates.removeListener(_update);
-    packs.removeListener(_update);
-    super.dispose();
-  }
+  List<String> getPopularCategories({int limit = 5}) =>
+      topCategories.keys.take(limit).toList();
 }
