e49d9850444944f489928e11017e59ea15348129
ClubBoss <esalimzade@hotmail.com>
2025-07-17 12:50:31 +0400

    feat: add YAML pack preview engine

--- lib/screens/dev_menu_screen.dart
+++ lib/screens/dev_menu_screen.dart
@@ -40,6 +40,7 @@ import '../core/training/generation/yaml_reader.dart';
 import 'package:file_picker/file_picker.dart';
 import 'pack_matrix_config_editor_screen.dart';
 import 'yaml_library_preview_screen.dart';
+import 'yaml_pack_quick_preview_screen.dart';
 import 'pack_library_health_screen.dart';
 import 'pack_library_stats_screen.dart';
 import 'pack_filter_debug_screen.dart';
@@ -838,6 +839,18 @@ class _DevMenuScreenState extends State<DevMenuScreen> {
                     context,
                     MaterialPageRoute(
                       builder: (_) => const YamlLibraryPreviewScreen(),
+                    ),
+                  );
+                },
+              ),
+            if (kDebugMode)
+              ListTile(
+                title: const Text('📦 Быстрый просмотр YAML'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const YamlPackQuickPreviewScreen(),
                     ),
                   );
                 },
