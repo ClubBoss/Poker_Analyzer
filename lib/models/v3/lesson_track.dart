50010f6b40fff10dce55bf5f57d140007af87ea6
ClubBoss <esalimzade@hotmail.com>
2025-07-19 05:37:12 +0400

    Add LessonTrack model and LearningTrackEngine

---
+++ lib/models/v3/lesson_track.dart
@@ -0,0 +1,13 @@
+class LessonTrack {
+  final String id;
+  final String title;
+  final String description;
+  final List<String> stepIds;
+
+  const LessonTrack({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.stepIds,
+  });
+}
