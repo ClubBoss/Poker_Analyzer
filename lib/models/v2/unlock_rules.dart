c4e628247aca94ada10ed9dfc345e16a0f721ae2
ClubBoss <esalimzade@hotmail.com>
2025-07-20 00:53:04 +0400

    feat: show locked library packs

--- lib/models/v2/unlock_rules.dart
+++ lib/models/v2/unlock_rules.dart
@@ -2,17 +2,20 @@ class UnlockRules {
   final List<String> requiredPacks;
   final double? minEV;
   final bool? requiresStarterPathCompleted;
+  final String? unlockHint;
 
   const UnlockRules({
     this.requiredPacks = const [],
     this.minEV,
     this.requiresStarterPathCompleted,
+    this.unlockHint,
   });
 
   factory UnlockRules.fromJson(Map<String, dynamic> j) => UnlockRules(
         requiredPacks: [for (final p in (j['requiredPacks'] as List? ?? [])) p.toString()],
         minEV: (j['minEV'] as num?)?.toDouble(),
         requiresStarterPathCompleted: j['requiresStarterPathCompleted'] as bool?,
+        unlockHint: j['unlockHint'] as String?,
       );
 
   Map<String, dynamic> toJson() => {
@@ -20,5 +23,6 @@ class UnlockRules {
         if (minEV != null) 'minEV': minEV,
         if (requiresStarterPathCompleted != null)
           'requiresStarterPathCompleted': requiresStarterPathCompleted,
+        if (unlockHint != null && unlockHint!.isNotEmpty) 'unlockHint': unlockHint,
       };
 }
