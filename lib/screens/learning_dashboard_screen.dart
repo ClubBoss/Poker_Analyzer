f3ab8f73c75ab5f317d398717f75b8908b3328c9
ClubBoss <esalimzade@hotmail.com>
2025-07-22 10:53:58 +0400

    feat: add ReviewPathCard for scheduled recovery

--- lib/screens/learning_dashboard_screen.dart
+++ lib/screens/learning_dashboard_screen.dart
@@ -18,6 +18,7 @@ import '../widgets/feed_recommendation_widget.dart';
 import '../widgets/next_up_banner.dart';
 import '../widgets/skill_loss_banner.dart';
 import '../widgets/tag_insight_reminder_card.dart';
+import '../widgets/review_path_card.dart';
 import '../models/training_attempt.dart';
 import '../models/v2/training_pack_template_v2.dart';
 import '../theme/app_colors.dart';
@@ -238,6 +239,8 @@ class _LearningDashboardScreenState extends State<LearningDashboardScreen> {
                 const NextUpBanner(),
                 const SizedBox(height: 12),
               ],
+              const ReviewPathCard(),
+              const SizedBox(height: 12),
               _section('🎯 Completion', '$completion% complete'),
               const SizedBox(height: 12),
               _improvements(data.improvements),
