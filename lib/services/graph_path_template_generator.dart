891668c140e196921f8bc1d12a626fd9d5329550
ClubBoss <esalimzade@hotmail.com>
2025-07-27 19:48:58 +0400

    feat: add graph template library

--- lib/services/graph_path_template_generator.dart
+++ lib/services/graph_path_template_generator.dart
@@ -77,4 +77,78 @@ class GraphPathTemplateGenerator {
     };
     return const YamlEncoder().convert(map);
   }
+
+  /// Returns a simple ICM introduction graph template.
+  String generateIcmIntroTemplate() {
+    final map = {
+      'nodes': [
+        {
+          'type': 'stage',
+          'id': 'start',
+          'next': ['knowledge']
+        },
+        {
+          'type': 'branch',
+          'id': 'knowledge',
+          'prompt': 'ICM experience',
+          'branches': {
+            'Beginner': 'icm_basics',
+            'Advanced': 'icm_advanced'
+          }
+        },
+        {
+          'type': 'stage',
+          'id': 'icm_basics',
+          'next': ['end']
+        },
+        {
+          'type': 'stage',
+          'id': 'icm_advanced',
+          'next': ['end']
+        },
+        {
+          'type': 'stage',
+          'id': 'end'
+        }
+      ]
+    };
+    return const YamlEncoder().convert(map);
+  }
+
+  /// Returns a simple Heads-Up introduction graph template.
+  String generateHeadsUpIntroTemplate() {
+    final map = {
+      'nodes': [
+        {
+          'type': 'stage',
+          'id': 'start',
+          'next': ['experience']
+        },
+        {
+          'type': 'branch',
+          'id': 'experience',
+          'prompt': 'Heads-Up experience',
+          'branches': {
+            'New': 'hu_basics',
+            'Grinder': 'hu_strategy'
+          }
+        },
+        {
+          'type': 'stage',
+          'id': 'hu_basics',
+          'next': ['end']
+        },
+        {
+          'type': 'stage',
+          'id': 'hu_strategy',
+          'next': ['end']
+        },
+        {
+          'type': 'stage',
+          'id': 'end'
+        }
+      ]
+    };
+    return const YamlEncoder().convert(map);
+  }
 }
