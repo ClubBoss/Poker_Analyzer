c2b45eeee7080c6a7d789cc6e3cb81f2afafd9de
ClubBoss <esalimzade@hotmail.com>
2025-07-31 11:17:49 +0400

    feat: add theory recall overlay scheduler

--- lib/app_providers.dart
+++ lib/app_providers.dart
@@ -128,6 +128,7 @@ import 'services/smart_recap_banner_reinjection_service.dart';
 import 'services/recap_to_drill_launcher.dart';
 import 'services/smart_booster_unlock_scheduler.dart';
 import 'services/overlay_booster_manager.dart';
+import 'services/theory_recall_overlay_scheduler.dart';
 import 'services/adaptive_next_step_engine.dart';
 import 'services/suggested_next_step_engine.dart';
 
@@ -625,6 +626,9 @@ List<SingleChildWidget> buildTrainingProviders() {
     ),
     Provider(
       create: (_) => OverlayBoosterManager()..start(),
+    ),
+    Provider(
+      create: (_) => TheoryRecallOverlayScheduler()..start(),
     ),
   ];
 }
