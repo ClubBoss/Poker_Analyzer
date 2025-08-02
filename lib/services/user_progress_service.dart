44a7434a91641cb5a6fd93bd7f67f3edcf0855dd
ClubBoss <esalimzade@hotmail.com>
2025-08-02 21:50:30 +0400

    feat: add theory block completion evaluator

---
+++ lib/services/user_progress_service.dart
@@ -0,0 +1,17 @@
+import 'mini_lesson_progress_tracker.dart';
+import 'pack_library_completion_service.dart';
+
+/// Provides completion lookups for theory lessons and training packs.
+class UserProgressService {
+  UserProgressService._();
+  static final instance = UserProgressService._();
+
+  Future<bool> isTheoryLessonCompleted(String id) {
+    return MiniLessonProgressTracker.instance.isCompleted(id);
+  }
+
+  Future<bool> isPackCompleted(String id) async {
+    final data = await PackLibraryCompletionService.instance.getCompletion(id);
+    return data != null;
+  }
+}
