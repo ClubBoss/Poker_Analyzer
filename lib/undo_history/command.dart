0ad75d0e21b237807788d88f7c080fa470d43254
ClubBoss <esalimzade@hotmail.com>
2025-08-03 20:33:48 +0400

    feat: make command immutable

--- lib/undo_history/command.dart
+++ lib/undo_history/command.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:collection/collection.dart';
+
+@immutable
 class Command {
   final String type;
   final Map<String, dynamic>? payload;
   final DateTime time;
 
-  Command(this.type, {this.payload}) : time = DateTime.now();
+  const Command(
+    this.type, {
+    this.payload,
+    required this.time,
+  });
 
   Map<String, dynamic> toJson() => {
         'type': type,
@@ -14,5 +22,20 @@ class Command {
   factory Command.fromJson(Map<String, dynamic> json) => Command(
         json['type'] as String,
         payload: (json['payload'] as Map?)?.cast<String, dynamic>(),
+        time: DateTime.parse(json['time'] as String),
       );
+
+  static const _mapEquality = MapEquality<String, dynamic>();
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Command &&
+          runtimeType == other.runtimeType &&
+          type == other.type &&
+          _mapEquality.equals(payload, other.payload) &&
+          time == other.time;
+
+  @override
+  int get hashCode => Object.hash(type, _mapEquality.hash(payload), time);
 }
