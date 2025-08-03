7597dd2606a6bd774b4e98cdcd26d531c97abb36
ClubBoss <esalimzade@hotmail.com>
2025-08-04 03:37:23 +0400

    feat: add app info service

--- lib/providers/core_providers.dart
+++ lib/providers/core_providers.dart
@@ -6,6 +6,7 @@ import '../services/remote_config_service.dart';
 import '../services/ab_test_engine.dart';
 import '../services/theme_service.dart';
 import '../services/cloud_sync_service.dart';
+import '../services/app_info_service.dart';
 import '../widgets/player_zone_widget.dart';
 import 'provider_globals.dart';
 import '../utils/loadable_extension.dart';
@@ -19,5 +20,6 @@ List<SingleChildWidget> buildCoreProviders(CloudSyncService cloud) {
     ChangeNotifierProvider(create: (_) => ThemeService()..init()),
     Provider<CloudSyncService>.value(value: cloud),
     Provider(create: (_) => PlayerZoneRegistry()),
+    Provider(create: (_) => AppInfoService()),
   ];
 }
