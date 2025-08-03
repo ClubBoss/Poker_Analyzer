eb5ce572aac70dc59b68674ad4f0cd0f2e0516db
ClubBoss <esalimzade@hotmail.com>
2025-08-03 06:36:31 +0400

    feat: add inbox booster limiter service

--- lib/services/smart_inbox_controller.dart
+++ lib/services/smart_inbox_controller.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/widgets.dart';
 
 import '../widgets/inbox_pinned_block_booster_banner.dart';
+import 'smart_booster_inbox_limiter_service.dart';
 import 'smart_pinned_block_booster_provider.dart';
 
 /// Aggregates smart inbox items for the user.
 class SmartInboxController {
-  SmartInboxController({SmartPinnedBlockBoosterProvider? boosterProvider})
-    : boosterProvider = boosterProvider ?? SmartPinnedBlockBoosterProvider();
+  SmartInboxController({
+    SmartPinnedBlockBoosterProvider? boosterProvider,
+    SmartBoosterInboxLimiterService? inboxLimiter,
+  })  : boosterProvider = boosterProvider ?? SmartPinnedBlockBoosterProvider(),
+        inboxLimiter = inboxLimiter ?? SmartBoosterInboxLimiterService();
 
   final SmartPinnedBlockBoosterProvider boosterProvider;
+  final SmartBoosterInboxLimiterService inboxLimiter;
 
   /// Returns widgets to display in the smart inbox.
   Future<List<Widget>> getInboxItems() async {
     final items = <Widget>[];
     final boosters = await boosterProvider.getBoosters();
     if (boosters.isNotEmpty) {
-      items.add(
-        InboxPinnedBlockBoosterBanner(suggestions: boosters.take(2).toList()),
-      );
+      final allowed = <PinnedBlockBoosterSuggestion>[];
+      for (final b in boosters) {
+        if (await inboxLimiter.canShow(b.tag)) {
+          await inboxLimiter.recordShown(b.tag);
+          allowed.add(b);
+        }
+        if (allowed.length >= SmartBoosterInboxLimiterService.maxPerDay) break;
+      }
+      if (allowed.isNotEmpty) {
+        items.add(InboxPinnedBlockBoosterBanner(suggestions: allowed));
+      }
     }
     return items;
   }
