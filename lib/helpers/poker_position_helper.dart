03efcfc33a183084aecb989c6fa392ebfd937db8
ClubBoss <esalimzade@hotmail.com>
2025-08-03 20:59:42 +0400

    refactor: extract positions map

--- lib/helpers/poker_position_helper.dart
+++ lib/helpers/poker_position_helper.dart
@@ -1,3 +1,15 @@
+const Map<int, List<String>> positionsByCount = {
+  2: ['SB', 'BB'],
+  3: ['BTN', 'SB', 'BB'],
+  4: ['CO', 'BTN', 'SB', 'BB'],
+  5: ['MP', 'CO', 'BTN', 'SB', 'BB'],
+  6: ['UTG', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
+  7: ['UTG', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
+  8: ['UTG', 'UTG+1', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
+  9: ['UTG', 'UTG+1', 'UTG+2', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
+  10: ['UTG', 'UTG+1', 'UTG+2', 'UTG+3', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
+};
+
 /// Returns the list of position names for a given number of players.
 ///
 /// The list is ordered from UTG to BB and supports tables with 2 to 10 players.
@@ -6,18 +18,6 @@ List<String> getPositionList(int playerCount) {
   if (playerCount < 2 || playerCount > 10) {
     throw ArgumentError('Supported range: 2 to 10 players');
   }
-
-  const Map<int, List<String>> positionsByCount = {
-    2: ['SB', 'BB'],
-    3: ['BTN', 'SB', 'BB'],
-    4: ['CO', 'BTN', 'SB', 'BB'],
-    5: ['MP', 'CO', 'BTN', 'SB', 'BB'],
-    6: ['UTG', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
-    7: ['UTG', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
-    8: ['UTG', 'UTG+1', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
-    9: ['UTG', 'UTG+1', 'UTG+2', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
-    10: ['UTG', 'UTG+1', 'UTG+2', 'UTG+3', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB'],
-  };
 
   return positionsByCount[playerCount]!;
 }
