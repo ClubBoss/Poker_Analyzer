91c3fb01c5159bf18b96a2ad0eb5da589eee0a69
ClubBoss <esalimzade@hotmail.com>
2025-08-04 21:32:23 +0400

    Add theory engagement analytics service

---
+++ lib/models/theory_lesson_engagement_stats.dart
@@ -0,0 +1,14 @@
+class TheoryLessonEngagementStats {
+  final String lessonId;
+  final int manualOpens;
+  final int reviewViews;
+  final double successRate;
+
+  const TheoryLessonEngagementStats({
+    required this.lessonId,
+    required this.manualOpens,
+    required this.reviewViews,
+    required this.successRate,
+  });
+}
+
