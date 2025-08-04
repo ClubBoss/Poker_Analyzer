e6f79e5d100fd19d34fac649e4f407cd64832ec2
ClubBoss <esalimzade@hotmail.com>
2025-08-04 05:27:11 +0400

    feat: add pack library service

---
+++ lib/generated/pack_library.g.dart
@@ -0,0 +1,7 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'dart:convert';
+
+import 'package:poker_analyzer/models/v2/training_pack_spot.dart';
+
+final Map<String, List<TrainingPackSpot>> packLibrary = {};
