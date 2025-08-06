3448bbf8b04d93ea6998a97afceca3d5822c47e3
ClubBoss <esalimzade@hotmail.com>
2025-08-06 05:46:32 +0400

    chore(android): safely load local properties

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,7 +1,10 @@
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
+        val localProperties = File(rootDir, "local.properties")
+        if (localProperties.exists()) {
+            localProperties.inputStream().use { properties.load(it) }
+        }
         val flutterSdkPath = properties.getProperty("flutter.sdk")
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
