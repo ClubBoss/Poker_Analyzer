c8b49fb2d8b52cabbfe1673be0b8275aa0d75334
ClubBoss <esalimzade@hotmail.com>
2025-08-07 10:15:27 +0400

    feat: classify autogen pack errors

---
+++ test/services/autogen_pack_error_classifier_service_test.dart
@@ -0,0 +1,43 @@
+import 'package:test/test.dart';
+import 'package:poker_analyzer/services/autogen_pack_error_classifier_service.dart';
+import 'package:poker_analyzer/models/v2/training_pack_template_v2.dart';
+import 'package:poker_analyzer/models/game_type.dart';
+import 'package:poker_analyzer/core/training/engine/training_type_engine.dart';
+
+TrainingPackTemplateV2 _emptyPack() => TrainingPackTemplateV2(
+      id: 'id',
+      name: 'name',
+      trainingType: TrainingType.custom,
+      spots: [],
+      spotCount: 0,
+      tags: [],
+      gameType: GameType.cash,
+      bb: 0,
+      positions: [],
+      meta: {},
+    );
+
+void main() {
+  group('AutogenPackErrorClassifierService', () {
+    const classifier = AutogenPackErrorClassifierService();
+
+    test('detects no spots generated', () {
+      final type = classifier.classify(_emptyPack(), null);
+      expect(type, AutogenPackErrorType.noSpotsGenerated);
+    });
+
+    test('detects duplicate error', () {
+      final type =
+          classifier.classify(_emptyPack(), Exception('duplicate spot'));
+      expect(type, AutogenPackErrorType.duplicate);
+    });
+
+    test('detects invalid board', () {
+      final type = classifier.classify(
+        _emptyPack(),
+        Exception('Invalid board sequence'),
+      );
+      expect(type, AutogenPackErrorType.invalidBoard);
+    });
+  });
+}
