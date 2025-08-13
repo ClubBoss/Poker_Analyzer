35df468f588cb93b6f23487f3c7f2419fed0610c
ClubBoss <esalimzade@hotmail.com>
2025-08-14 00:53:37 +0400

    feat: add mix key normalization

--- test/l3_cli_runner_weights_parse_test.dart
+++ test/l3_cli_runner_weights_parse_test.dart
@@ -5,17 +5,30 @@ import 'package:poker_analyzer/services/l3_cli_runner.dart';
 
 void main() {
   test('extractTargetMix parses inline json', () {
-    final mix = extractTargetMix('{"targetMix":{"rainbow":0.2}}');
-    expect(mix, isNotNull);
-    expect(mix!['rainbow'], 0.2);
+    final res = extractTargetMix('{"targetMix":{"rainbow":0.2}}');
+    expect(res, isNotNull);
+    expect(res!.mix['rainbow'], 0.2);
+    expect(res.tolerance, 0.10);
   });
 
-  test('extractTargetMix parses file path', () {
+  test('extractTargetMix parses file path with tolerance', () {
     final file = File('${Directory.systemTemp.path}/weights.json');
-    file.writeAsStringSync('{"targetMix":{"monotone":0.3}}');
-    final mix = extractTargetMix(file.path);
-    expect(mix, isNotNull);
-    expect(mix!['monotone'], 0.3);
+    file.writeAsStringSync(
+      '{"targetMix":{"monotone":0.3},"mixTolerance":0.05}',
+    );
+    final res = extractTargetMix(file.path);
+    expect(res, isNotNull);
+    expect(res!.mix['monotone'], 0.3);
+    expect(res.tolerance, 0.05);
     file.deleteSync();
+  });
+
+  test('extractTargetMix normalizes keys', () {
+    final res = extractTargetMix(
+      '{"targetMix":{"two_tone":0.3,"broadway":0.25}}',
+    );
+    expect(res, isNotNull);
+    expect(res!.mix.containsKey('twoTone'), isTrue);
+    expect(res.mix.containsKey('broadwayHeavy'), isTrue);
   });
 }
