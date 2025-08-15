5ed3cf4365d134171146a344b5e833b86cfbef53
ClubBoss <esalimzade@hotmail.com>
2025-08-15 09:27:56 +0400

    chore: fix generator initializer and ignore long lines

--- lib/generated/pack_library.g.dart
+++ lib/generated/pack_library.g.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, lines_longer_than_80_chars
 
 import 'dart:convert';
 import 'package:poker_analyzer/models/v2/training_pack_spot.dart';
