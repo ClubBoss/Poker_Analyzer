375cc3ac9a20d148a2bc6052189acbdfe78ebe3c
ClubBoss <esalimzade@hotmail.com>
2025-08-17 01:46:14 +0400

    feat: add river jam vs raise spot kind

--- lib/ui/session_player/models.dart
+++ lib/ui/session_player/models.dart
@@ -15,10 +15,10 @@ enum SpotKind {
   l3_probe_jam_vs_raise,
   l3_river_jam_vs_bet,
   l3_turn_jam_vs_bet,
-  l3_river_jam_vs_raise,
   l3_flop_jam_vs_bet,
   l3_flop_jam_vs_raise,
   l3_turn_jam_vs_raise,
+  l3_river_jam_vs_raise,
 }
 
 class UiSpot {
