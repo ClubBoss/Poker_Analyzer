1cbe714d1c447d4a013fef22f0fd94f37b43c3f0
ClubBoss <esalimzade@hotmail.com>
2025-08-18 04:44:32 +0400

    Add L4 ICM BB jam vs fold kind

--- lib/ui/session_player/models.dart
+++ lib/ui/session_player/models.dart
@@ -22,6 +22,7 @@ enum SpotKind {
   l4_icm_bubble_jam_vs_fold,
   l4_icm_ladder_jam_vs_fold,
   l4_icm_sb_jam_vs_fold,
+  l4_icm_bb_jam_vs_fold,
 }
 
 const _spotKindBaseline = [
@@ -48,6 +49,7 @@ const _spotKindBaseline = [
   'l4_icm_bubble_jam_vs_fold',
   'l4_icm_ladder_jam_vs_fold',
   'l4_icm_sb_jam_vs_fold',
+  'l4_icm_bb_jam_vs_fold',
 ];
 
 final _spotKindGuard = () {
