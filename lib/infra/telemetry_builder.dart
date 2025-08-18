fdf313754a6f41563ca9f6ff01f5c132247f98d2
ClubBoss <esalimzade@hotmail.com>
2025-08-18 20:03:52 +0400

    feat: add telemetry payload builders

--- lib/infra/telemetry_builder.dart
+++ lib/infra/telemetry_builder.dart
@@ -16,3 +16,41 @@ Map<String, dynamic> buildTelemetry({
   }
   return m;
 }
+
+Map<String, Object?> buildTheoryViewed({
+  required String moduleId,
+  required String theoryId,
+}) {
+  return {'moduleId': moduleId, 'theoryId': theoryId};
+}
+
+Map<String, Object?> buildDemoCompleted({
+  required String moduleId,
+  required String demoId,
+  int? ms,
+}) {
+  final m = <String, Object?>{'moduleId': moduleId, 'demoId': demoId};
+  if (ms != null) m['ms'] = ms;
+  return m;
+}
+
+Map<String, Object?> buildPracticeSpotAnswered({
+  required String moduleId,
+  required String spotId,
+  required bool correct,
+  required int ms,
+  String? kind,
+}) {
+  final m = <String, Object?>{
+    'moduleId': moduleId,
+    'spotId': spotId,
+    'correct': correct,
+    'ms': ms,
+  };
+  if (kind != null) m['kind'] = kind;
+  return m;
+}
+
+Map<String, Object?> buildModuleMastered({required String moduleId}) {
+  return {'moduleId': moduleId};
+}
